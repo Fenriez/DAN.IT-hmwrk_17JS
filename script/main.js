454f0820d23befa364f88e5e1f500bbbe4305982
Fenriez <andy.dolot@gmail.com>
2019-01-03 12:40:00 +0200

    In progress

---
+++ script/main.js
@@ -0,0 +1,29 @@
+let size, color;
+
+$('.btn').on('click', function() {
+    // console.log($(this));
+    $('<input type="text" class="size_input">').appendTo('body');    
+    $('<input type="text" class="color_input">').appendTo('body');
+    $('<button class="btn draw">').appendTo('body');
+    
+    $('.size_input').on('change', sizeInput);
+    $('.size_input').on('change', colorInput);
+    $('.draw').text('Нарисовать').on('click', drawCircle);
+    
+    $(this).detach();
+})
+
+function sizeInput(event) {
+    size = event.target.value + "px";
+}
+function colorInput(event) {
+    color = event.target.value;
+}
+function drawCircle() {
+    $('body > *').detach();
+    $('<div>').css({
+        height: size,
+        width: size,
+        background: color
+    }).appendTo('body');
+}
